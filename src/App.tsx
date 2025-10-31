a82b7080432c1e46b3f82e9a38d25542c22acd72
Sahil Singh <s.sahil3024@gmail.com>
2025-10-31 18:24:56 +0530

    Header and layout done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router'
 import FeedPage from './pages/Feed'
 import SigninPage from './pages/Signin'
 import SignupPage from './pages/Signup'
+import Layout from './pages/Layout'
 function App() {
   return (
     <>
       <Routes>
-        <Route index element={<FeedPage />} />
-        <Route path="signin" element={<SigninPage />} />
-        <Route path="signup" element={<SignupPage />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<FeedPage />} />
+          <Route path="signin" element={<SigninPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Route>
 
       </Routes>
     </>
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,36 @@
+import { Link, Outlet, useLocation } from 'react-router';
+import { ReactComponent as Logo } from '../assets/logo.svg'
+import { ReactComponent as Login } from '../assets/login.svg'
+
+function Layout() {
+    const location = useLocation()
+    const isHomePage = location.pathname == "/"
+    return (
+        <div>
+            <header className="p-8 flex justify-between">
+                <Link to={"/"}>
+                    <div className='font-bold flex flex-row items-center gap-1'>
+                        <Logo />
+                        foo-rum
+                    </div>
+                </Link>
+
+                <Link to={isHomePage ? "/signin" : "/"}>
+                    <div className='font-semibold text-sm flex flex-row items-center gap-1 pt-1'>
+                        {
+                            isHomePage ?
+                                <>Login<Login /></> :
+                                "Back to home"
+                        }
+
+                    </div>
+                </Link>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default Layout;
